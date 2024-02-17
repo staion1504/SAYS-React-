f64a820779ac426c1b569030191b008f046d8499
staion1504 <pagillasaitejauma@gmail.com>
2024-02-17 22:16:55 +0530

    added custom middleware in sginup(user and theatre)

--- Server/routes/Theatre/Tsignup_routes.js
+++ Server/routes/Theatre/Tsignup_routes.js
@@ -5,216 +5,224 @@ const md5 = require("md5");
 
 //Database
 const theatresignupinfo = require("../../models/theatre/signup");
-const theatreverification=require("../../models/Admin/theatreverification");
+const theatreverification = require("../../models/Admin/theatreverification");
 
 router.get("/", function (req, res) {
-    const show_error = req.cookies.show_error;
-    res.clearCookie("show_error");
-    res.render("TSignup", { show_error: show_error });
-  });
+  const show_error = req.cookies.show_error;
+  res.clearCookie("show_error");
+  res.render("TSignup", { show_error: show_error });
+});
 
-router.post("/", function (req, res) {
+// Custom Middleware
+const validation = (req, res, next) => {
+  
+  let Theatre_Name = req.body.Theatre_Name;
+  let email = req.body.email;
+  let num1 = req.body.Contact_Number1;
+  let num2 = req.body.Contact_Number2;
+  let street = req.body.Street;
+  let City = req.body.City;
+  let State = req.body.State;
+  let Pincode = req.body.Pincode;
+  let License_Number = req.body.License_Number;
+  let Login_password = req.body.Login_password;
+  let nearbyplace1 = req.body.nearbyplace1;
+  let nearbyplace2 = req.body.nearbyplace2;
+  let nearbyplace3 = req.body.nearbyplace3;
+  let nearbyplace4 = req.body.nearbyplace4;
 
-    // console.log(req.body);
+  const phoneRegex = /^\d{10}$/;
+  const pincoderegex = /^[1-9][0-9]{5}$/;
+  var License_Numberregex = /^\w{6}$/;
 
-    
+  let flag = false;
+  let row;
 
-    let Theatre_Name = req.body.Theatre_Name;
-    let email = req.body.email;
-    let num1 = req.body.Contact_Number1;
-    let num2 = req.body.Contact_Number2;
-    let street = req.body.Street;
-    let City = req.body.City;
-    let State = req.body.State;
-    let Pincode = req.body.Pincode;
-    let License_Number = req.body.License_Number;
-    let Login_password = req.body.Login_password;
-    let nearbyplace1 = req.body.nearbyplace1;
-    let nearbyplace2 = req.body.nearbyplace2;
-    let nearbyplace3 = req.body.nearbyplace3;
-    let nearbyplace4 = req.body.nearbyplace4;
-  
-    const phoneRegex = /^\d{10}$/;
-    const pincoderegex = /^[1-9][0-9]{5}$/;
-    var License_Numberregex = /^\w{6}$/;
-  
-    let flag = false;
-    let row;
-  
-    let email_len = email.length;
-  
-    if (email_len < 10 && !flag) {
-      res.json( "Email should be greater than length 10");
-      flag = true;
-    
-    }
-  
-    let i = email_len - 1;
-    let str = "";
-  
-    for (let j = 0; j < 10; j++) {
-      str += email.charAt(i);
-      i--;
-    }
-    str = str.split("").reverse().join("");
-  
-    if (str != "@gmail.com" && !flag) {
-      res.json( "Email should have domain as @gmail.com");
-      flag = true;
-      
-    }
-  
-    if (!phoneRegex.test(num1) && !flag) {
-      res.json(  "Enter correct Mobile number");
-      flag = true;
-     
-    }
-  
-    if (!phoneRegex.test(num2) && !flag) {
-      res.json(  "Enter correct Mobile number");
-      flag = true;
-      
-    }
-    if (!pincoderegex.test(Pincode) && !flag) {
-      res.json( "Enter correct pinicode number");
-      flag = true;
-      
-    }
-    if (!License_Numberregex.test(License_Number) && !flag) {
-      res.json(  "Enter correct License Number of length 6");
-      flag = true;
-     
-    }
-    if (Login_password.includes("#")) {
-      res.json( 
-        "passwords should not contain # symbol for security"
-      );
-      flag = true;
-      
-    }
-    if (Login_password.length < 8 && !flag) {
-      res.json( "Login password should greater than length 8");
-      flag = true;
-     
-    }
-    if (
-      Login_password.charAt(0) != Login_password.charAt(0).toUpperCase() &&
-      !flag
-    ) {
-      res.json( 
-        "Login password first character should be capital"
-      );
-      flag = true;
-      
-    }
-    if (!flag) {
-      theatresignupinfo.find({ licensenum: License_Number }).then((value) => {
-        //    console.log(value);
-        if (value.length != 0) {
-          res.json(  "License Number already registered");
-          
-        } else {
-          theatresignupinfo.find({ email: email }).then((value) => {
-            if (value.length != 0) {
-              res.json(  "Email already registered");
-              
-            } else {
-              let TheatreReferenceNumber = "SAYSTheatre" + email;
-  
-              let a = [
-                TheatreReferenceNumber,
-                Theatre_Name,
-                email,
-                num1,
-                num2,
-                street,
-                City,
-                State,
-                Pincode,
-                License_Number,
-                md5(Login_password),
-                nearbyplace1,
-                nearbyplace2,
-                nearbyplace3,
-                nearbyplace4,
-              ];
-  
-              let b = [
-                "tReferenceNumber",
-                "tName",
-                "temail",
-                "tNumber1",
-                "tNumber2",
-                "street",
-                "city",
-                "state",
-                "pincode",
-                "licensenum",
-                "LoginPassword",
-                "nearbyplace1",
-                "nearbyplace2",
-                "nearbyplace3",
-                "nearbyplace4",
-              ];
-  
-              let registration = new theatreverification();
-  
-              for (let i = 0; i < b.length; i++) {
-                registration[b[i]] = a[i];
-              }
-  
-              registration
-                .save()
-                .then(() => {
-                  console.log(
-                    "Inserted TSignup data in  theatre verification database"
-                  );
-                  
-  
-                  let mailtransporter = nodemailer.createTransport({
-                    service: "gmail",
-                    auth: {
-                      user: "contactsays123@gmail.com",
-                      pass: "hhbemttmmjnxzfri",
-                    },
-                  });
-  
-                  let details = {
-                    from: "contactsays123@gmail.com",
-                    to: email,
-                    subject: "Theatre Sent for Verification",
-                    text: "Dear Theatre,Theatre Sent for Verification.It may take 2 or 3 Working Days.Please be Patient.",
-                  };
-                  mailtransporter.sendMail(details, (err) => {
-                    if (err) console.log(err);
-                    else {
-                      sentdata = "Email has been sent successfully";
-                      console.log("Email has sent!!!");
-                    }
-                  });
-  
-                  res.json( "/login");
-                })
-                .catch((err) => {
-                  console.log(err);
-                });
-            }
-          });
-        }
-      });
-    }
-  });
+  let email_len = email.length;
 
-  router.post("/checktheatre", function (req, res) {
-    let k=0;
-    let {email}=req.body;
-    theatresignupinfo.find({ temail: email }).then((value) => {
+  if (email_len < 10 && !flag) {
+    res.json("Email should be greater than length 10");
+    flag = true;
+
+  }
+
+  let i = email_len - 1;
+  let str = "";
+
+  for (let j = 0; j < 10; j++) {
+    str += email.charAt(i);
+    i--;
+  }
+  str = str.split("").reverse().join("");
+
+  if (str != "@gmail.com" && !flag) {
+    res.json("Email should have domain as @gmail.com");
+    flag = true;
+
+  }
+
+  if (!phoneRegex.test(num1) && !flag) {
+    res.json("Enter correct Mobile number");
+    flag = true;
+
+  }
+
+  if (!phoneRegex.test(num2) && !flag) {
+    res.json("Enter correct Mobile number");
+    flag = true;
+
+  }
+  if (!pincoderegex.test(Pincode) && !flag) {
+    res.json("Enter correct pinicode number");
+    flag = true;
+
+  }
+  if (!License_Numberregex.test(License_Number) && !flag) {
+    res.json("Enter correct License Number of length 6");
+    flag = true;
+
+  }
+  if (Login_password.includes("#")) {
+    res.json(
+      "passwords should not contain # symbol for security"
+    );
+    flag = true;
+
+  }
+  if (Login_password.length < 8 && !flag) {
+    res.json("Login password should greater than length 8");
+    flag = true;
+
+  }
+  if (
+    Login_password.charAt(0) != Login_password.charAt(0).toUpperCase() &&
+    !flag
+  ) {
+    res.json(
+      "Login password first character should be capital"
+    );
+    flag = true;
+
+  }
+  if (!flag) {
+    theatresignupinfo.find({ licensenum: License_Number }).then((value) => {
+      //    console.log(value);
       if (value.length != 0) {
-       k=1;
-      }
+        res.json("License Number already registered");
 
-      res.json({k:k});
-    })
-  });
+      } else {
+        theatresignupinfo.find({ email: email }).then((value) => {
+          if (value.length != 0) {
+            res.json("Email already registered");
+
+          } else {
+            let TheatreReferenceNumber = "SAYSTheatre" + email;
+
+            let a = [
+              TheatreReferenceNumber,
+              Theatre_Name,
+              email,
+              num1,
+              num2,
+              street,
+              City,
+              State,
+              Pincode,
+              License_Number,
+              md5(Login_password),
+              nearbyplace1,
+              nearbyplace2,
+              nearbyplace3,
+              nearbyplace4,
+            ];
+
+            let b = [
+              "tReferenceNumber",
+              "tName",
+              "temail",
+              "tNumber1",
+              "tNumber2",
+              "street",
+              "city",
+              "state",
+              "pincode",
+              "licensenum",
+              "LoginPassword",
+              "nearbyplace1",
+              "nearbyplace2",
+              "nearbyplace3",
+              "nearbyplace4",
+            ];
+
+            let registration = new theatreverification();
+
+            for (let i = 0; i < b.length; i++) {
+              registration[b[i]] = a[i];
+            }
+
+            registration
+              .save()
+              .then(() => {
+                console.log(
+                  "Inserted TSignup data in  theatre verification database"
+                );
+
+
+                let mailtransporter = nodemailer.createTransport({
+                  service: "gmail",
+                  auth: {
+                    user: "contactsays123@gmail.com",
+                    pass: "hhbemttmmjnxzfri",
+                  },
+                });
+
+                let details = {
+                  from: "contactsays123@gmail.com",
+                  to: email,
+                  subject: "Theatre Sent for Verification",
+                  text: "Dear Theatre,Theatre Sent for Verification.It may take 2 or 3 Working Days.Please be Patient.",
+                };
+                mailtransporter.sendMail(details, (err) => {
+                  if (err) console.log(err);
+                  else {
+                    sentdata = "Email has been sent successfully";
+                    console.log("Email has sent!!!");
+                  }
+                });
+
+                res.json("/login");
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+        });
+      }
+    });
+  }
+  next(); // Call the next middleware in the stack
+};
+
+router.use(validation);
+
+router.post("/",validation, function (req, res) {
+
+  // console.log(req.body);
+
+
+
+});
+
+router.post("/checktheatre", function (req, res) {
+  let k = 0;
+  let { email } = req.body;
+  theatresignupinfo.find({ temail: email }).then((value) => {
+    if (value.length != 0) {
+      k = 1;
+    }
+
+    res.json({ k: k });
+  })
+});
 
 module.exports = router;
--- Server/routes/user/signup.js
+++ Server/routes/user/signup.js
@@ -4,209 +4,221 @@ const userinfo = require("../../models/user/signup");
 const md5 = require("md5");
 
 router.get("/", function (req, res) {
-    const show_error = req.cookies.show_error;
-    res.clearCookie("show_error");
-  
-    res.render("Signup", { show_error: show_error });
-  });
+  const show_error = req.cookies.show_error;
+  res.clearCookie("show_error");
 
-  router.post("/", function (req, res) {
-       
-    // console.log("came");
-  
+  res.render("Signup", { show_error: show_error });
+});
 
-    let email = req.body.email;
-    let num = req.body.Mobile_Number;
-    let Login_password = req.body.Login_password;
-    let Profile_password = req.body.Profile_password;
-    let CardName = req.body.Card_Name;
-    let CardNum = req.body.Card_Number;
-    let CVV = req.body.CVV;
-    let Expiry = req.body.Expiry;
-    let DOB = req.body.DOB;
-    let gender = req.body.gender;
-  
-    //
-    const phoneRegex = /^\d{10}$/;
-    const alphabetRegex = /^[a-zA-Z]+$/;
-    const cardNumberRegex = /^\d{16}$/;
-    const numbersRegex = /^[0-9]{3}$/;
-    const numbersRegex2 = /^[0-9]{2}$/;
-    let flag = false;
-    let row;
-  
-    let email_len = email.length;
-  
-    if (email_len < 10 && !flag) {
-      res.json("Email should be greater than length 10");
-      flag = true;
-    }
-  
-    let i = email_len - 1;
-    let str = "";
-  
-    for (let j = 0; j < 10; j++) {
-      str += email.charAt(i);
-      i--;
-    }
-    str = str.split("").reverse().join("");
-  
-    if (str != "@gmail.com" && !flag) {
-      res.json("Email should have domain as @gmail.com");
-      flag = true;
-    }
-  
-    if (!phoneRegex.test(num) && !flag) {
-      res.json( "Enter correct Mobile number");
-      flag = true;
- 
-    }
-  
-    if (Login_password.includes("#")) {
-      res.json(
-      
-        "passwords should not contain # symbol for security"
-      );
-      flag = true;
-      
-    }
-    if (Login_password.length < 8 && !flag) {
-      res.json( "Login password should greater than length 8");
-      flag = true;
-      
-    }
-    if (
-      Login_password.charAt(0) != Login_password.charAt(0).toUpperCase() &&
-      !flag
-    ) {
-      res.json(
-        
-        "Login password first character should be capital"
-      );
-      flag = true;
-    
-    }
-  
-    if (Profile_password.includes("#")) {
-      res.json(
-       
-        "passwords should not contain # symbol for security"
-      );
-      flag = true;
-      
-    }
-  
-    if (Profile_password.length < 8 && !flag) {
-      res.json( "Profile password should greater than length 8");
-      flag = true;
-     
-    }
-    if (
-      Profile_password.charAt(0) != Profile_password.charAt(0).toUpperCase() &&
-      !flag
-    ) {
-      res.json(
-        
-        "Profile password first character should be capital"
-      );
-      flag = true;
-      
-    }
-  
-    if (!alphabetRegex.test(CardName) && !flag) {
-      res.json("Enter correct Crad Holder Name");
-      flag = true;
-      
-    }
-  
-    if (!cardNumberRegex.test(CardNum) && !flag) {
-      res.json( "Enter correct 16 digit Crad Number");
-      flag = true;
-      
-    }
-  
-    if (!numbersRegex.test(CVV) && !flag) {
-      res.json( "Enter correct CVV");
-      flag = true;
-      
-    }
-    if (!numbersRegex2.test(Expiry) && !flag) {
-      res.json( "Enter correct two digit expiry year");
-      flag = true;
-      
-    }
-  
-    if (!flag) {
-      userinfo.find({ email: email }).then((value) => {
-        if (value.length != 0) {
-          res.json( "Email already registered");
-         
-        } else {
-          let UserReferenceNumber = "SAYSUSER" + email;
-  
-          let a = [
-            UserReferenceNumber,
-            req.body.First_Name,
-            req.body.Last_Name,
-            DOB,
-            email,
-            num,
-            gender,
-            md5(Login_password),
-            md5(Profile_password),
-            CardName,
-            CardNum,
-            md5(CVV),
-            md5(Expiry),
-          ];
-  
-          let b = [
-            "UserReferenceNumber",
-            "firstName",
-            "lastName",
-            "DOB",
-            "email",
-            "MobileNumber",
-            "Gender",
-            "LoginPassword",
-            "profilePassword",
-            "cardName",
-            "cardNumber",
-            "CVV",
-            "expiry",
-          ];
-  
-          let registration = new userinfo();
-  
-          for (let i = 0; i < b.length; i++) {  
-            registration[b[i]] = a[i];
-          }
-  
-          registration
-            .save()
-            .then(() => {
-              console.log("Inserted Signup data in database");
-              res.json("/login");
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        }
-      });
-    }
-  });
 
-  router.post("/checkuser", function (req, res) {
-    let k=0;
-    let {email}=req.body;
+// Custom Middleware
+const validation = (req, res, next) => {
+  let email = req.body.email;
+  let num = req.body.Mobile_Number;
+  let Login_password = req.body.Login_password;
+  let Profile_password = req.body.Profile_password;
+  let CardName = req.body.Card_Name;
+  let CardNum = req.body.Card_Number;
+  let CVV = req.body.CVV;
+  let Expiry = req.body.Expiry;
+  let DOB = req.body.DOB;
+  let gender = req.body.gender;
+
+  //
+  const phoneRegex = /^\d{10}$/;
+  const alphabetRegex = /^[a-zA-Z]+$/;
+  const cardNumberRegex = /^\d{16}$/;
+  const numbersRegex = /^[0-9]{3}$/;
+  const numbersRegex2 = /^[0-9]{2}$/;
+  let flag = false;
+  let row;
+
+  let email_len = email.length;
+
+  if (email_len < 10 && !flag) {
+    res.json("Email should be greater than length 10");
+    flag = true;
+  }
+
+  let i = email_len - 1;
+  let str = "";
+
+  for (let j = 0; j < 10; j++) {
+    str += email.charAt(i);
+    i--;
+  }
+  str = str.split("").reverse().join("");
+
+  if (str != "@gmail.com" && !flag) {
+    res.json("Email should have domain as @gmail.com");
+    flag = true;
+  }
+
+  if (!phoneRegex.test(num) && !flag) {
+    res.json("Enter correct Mobile number");
+    flag = true;
+
+  }
+
+  if (Login_password.includes("#")) {
+    res.json(
+
+      "passwords should not contain # symbol for security"
+    );
+    flag = true;
+
+  }
+  if (Login_password.length < 8 && !flag) {
+    res.json("Login password should greater than length 8");
+    flag = true;
+
+  }
+  if (
+    Login_password.charAt(0) != Login_password.charAt(0).toUpperCase() &&
+    !flag
+  ) {
+    res.json(
+
+      "Login password first character should be capital"
+    );
+    flag = true;
+
+  }
+
+  if (Profile_password.includes("#")) {
+    res.json(
+
+      "passwords should not contain # symbol for security"
+    );
+    flag = true;
+
+  }
+
+  if (Profile_password.length < 8 && !flag) {
+    res.json("Profile password should greater than length 8");
+    flag = true;
+
+  }
+  if (
+    Profile_password.charAt(0) != Profile_password.charAt(0).toUpperCase() &&
+    !flag
+  ) {
+    res.json(
+
+      "Profile password first character should be capital"
+    );
+    flag = true;
+
+  }
+
+  if (!alphabetRegex.test(CardName) && !flag) {
+    res.json("Enter correct Crad Holder Name");
+    flag = true;
+
+  }
+
+  if (!cardNumberRegex.test(CardNum) && !flag) {
+    res.json("Enter correct 16 digit Crad Number");
+    flag = true;
+
+  }
+
+  if (!numbersRegex.test(CVV) && !flag) {
+    res.json("Enter correct CVV");
+    flag = true;
+
+  }
+  if (!numbersRegex2.test(Expiry) && !flag) {
+    res.json("Enter correct two digit expiry year");
+    flag = true;
+
+  }
+
+  if (!flag) {
     userinfo.find({ email: email }).then((value) => {
       if (value.length != 0) {
-       k=1;
-      }
+        res.json("Email already registered");
 
-      res.json({k:k});
-    })
-  });
+      } else {
+        let UserReferenceNumber = "SAYSUSER" + email;
+
+        let a = [
+          UserReferenceNumber,
+          req.body.First_Name,
+          req.body.Last_Name,
+          DOB,
+          email,
+          num,
+          gender,
+          md5(Login_password),
+          md5(Profile_password),
+          CardName,
+          CardNum,
+          md5(CVV),
+          md5(Expiry),
+        ];
+
+        let b = [
+          "UserReferenceNumber",
+          "firstName",
+          "lastName",
+          "DOB",
+          "email",
+          "MobileNumber",
+          "Gender",
+          "LoginPassword",
+          "profilePassword",
+          "cardName",
+          "cardNumber",
+          "CVV",
+          "expiry",
+        ];
+
+        let registration = new userinfo();
+
+        for (let i = 0; i < b.length; i++) {
+          registration[b[i]] = a[i];
+        }
+
+        registration
+          .save()
+          .then(() => {
+            console.log("Inserted Signup data in database");
+            res.json("/login");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    });
+  }
+  next(); // Call the next middleware in the stack
+};
+
+// Use the custom middleware for all routes
+router.use(validation);
+
+
+
+router.post("/", function (req, res) {
+
+  // console.log("came");
+
+
+
+});
+
+router.post("/checkuser", function (req, res) {
+  let k = 0;
+  let { email } = req.body;
+  userinfo.find({ email: email }).then((value) => {
+    if (value.length != 0) {
+      k = 1;
+    }
+
+    res.json({ k: k });
+  })
+});
 
 
 
