46e5bf46c9c7c05d7db7543bd2cbafdca656a5f2
Rupesh_peddineni <rupeshprofessional2728@gmail.com>
2024-02-22 00:31:18 +0530

    Homw page location based Theatre search done

--- React_SAYS/src/Components/Home/SecondSection/TheatreCard/TheatreCard.js
+++ React_SAYS/src/Components/Home/SecondSection/TheatreCard/TheatreCard.js
@@ -1,34 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classes from './TheatreCard.module.css';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
-const TheatreCard=()=>{
+const TheatreCard=({Tdetail})=>{
     return (
-     <div className={classes.card}>
-        <div className={classes.cardbody}>
-            <h2 className={classes.title}>PVR Cinemas</h2>
-             <div className={classes.specifications}>
-             <p>4k</p>
-             <p><FontAwesomeIcon icon={faCheck} style={{color: "#46d312",}} /> Snacks</p>
-             <p className={classes.type}>A/c</p>
-             </div>
-             <p className={classes.description}>The theatre is Located in mid city of Vijayawada with well furnished interiors and good sound system installed for better experience of the cinema</p>
-            <h5>Dolby Atmos</h5>
-        </div>
+    
+
+<div className={classes.card}>
+<div className={classes.cardbody}>
+    <h2 className={classes.title}>{Tdetail.tName}</h2>
+     <div className={classes.specifications}>
+     <p>{Tdetail.screentype}</p>
+     <p><FontAwesomeIcon icon={faCheck} style={{color: "#46d312",}} /> {Tdetail.snacks==="Available"? "Snacks":"No Snacks"}</p>
+     <p className={classes.type}>{Tdetail.Ttype}</p>
      </div>
+     <p className={classes.description}>{Tdetail.about}</p>
+    <h5>{Tdetail.sound}</h5>
+</div>
+</div>
      );
 }
 
-{/* <div className={classes.card} style={{backgroundImage:`url(${Tdetails.imgurl1})`}}>
-<div className={classes.cardbody}>
-    <h2 className={classes.title}>{Tdetails.tName}</h2>
-     <div className={classes.specifications}>
-     <p>{Tdetails.screentype}</p>
-     <p><FontAwesomeIcon icon={faCheck} style={{color: "#46d312",}} /> {Tdetails.snacks==="Available"? "Snacks":"No Snacks"}</p>
-     <p className={classes.type}>{Tdetails.Ttype}</p>
-     </div>
-     <p className={classes.description}>{Tdetails.about}</p>
-    <h5>{Tdetails.sound}</h5>
-</div>
-</div> */}
 
 export default TheatreCard;
