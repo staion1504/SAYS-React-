c9f6963e03ef5a8e4e9122b58973a7762b4c673b
Rupesh_peddineni <rupeshprofessional2728@gmail.com>
2024-02-23 11:57:52 +0530

    Profile pic navbar updated

--- React_SAYS/src/Components/Common/NavBar/Profile/ProfileDropDown.js
+++ React_SAYS/src/Components/Common/NavBar/Profile/ProfileDropDown.js
@@ -3,16 +3,41 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import profileimg from '../../../../Assests/NavBar/image.jpg';
 import classes from './ProfileDropDown.module.css';
 import { Link } from 'react-router-dom';
-
+import { useState,useEffect } from 'react';
 const ProfileDropDown=(props)=>{
-    const styleobj1=props.flag==="M"?{backgroundColor:"black"}:{backgroundColor:"#181616",height:"3.5rem"};
+
+  const [imageSrc, setImageSrc] = useState("https://png.pngtree.com/png-vector/20190501/ourmid/pngtree-users-icon-design-png-image_1014936.jpg");
+
+  const renderUserpic = async () => {
+    // console.log("hi");
+    const res = await fetch('http://localhost:5000/profile/profilepic', {
+      method: 'get',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      credentials: 'include',
+    });
+    const blob = await res.blob();
+    if (blob.size <= 3) {
+      return;
+    }
+    const imageUrl = URL.createObjectURL(blob);
+    setImageSrc(imageUrl);
+
+  }
+   
+  useEffect(() => {
+    renderUserpic();
+  }, []);
+
+      const styleobj1=props.flag==="M"?{backgroundColor:"black"}:{backgroundColor:"#181616",height:"3.5rem"};
 
      const styleobj2=props.flag==="M"?{marginTop:"1.5rem"}:{}               
     
     return ( <div className={classes.profilediv} style={styleobj2}>
         <Dropdown style={styleobj1}>   
            <Dropdown.Toggle id="dropdown-basic" variant='#181616' size='sm'>
-              <img src={profileimg} alt="mdo" className={classes.profile}/>
+              <img src={imageSrc} alt="mdo" className={classes.profile}/>
            </Dropdown.Toggle>
          
             <Dropdown.Menu>
