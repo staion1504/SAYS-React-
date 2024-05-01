22ea391280ac5ff69df9023a58ae90c51d254b30
Rupesh_peddineni <rupeshprofessional2728@gmail.com>
2024-05-02 02:09:19 +0530

    .env file added to store mongoURL for testing purpose

--- Server/server.test.js
+++ Server/server.test.js
@@ -5,21 +5,13 @@ const app = require('./server1.js')
 describe('POST auth', () => {
     describe('/login given mail and password', () => {
         it('should give 200 response on correct credentials ', async () => {
-            const response = await request(app).post("/log/login").send({
+            const response = await request(app).post("/login").send({
                 email: "surya@gmail.com",
                 password: "Surya@123"
               })
               expect(response.statusCode).toBe(200)
         },20000);
-        it('should give 400 response on incorrect credentials', async () => {
-            const response = await request(app).post("/log/login").send({
-                email: "surya12@gmail.com",
-                password: "pass@123"
-            })
-            expect(response.statusCode).toBe(400)
 
-        
-    })
 });
 })
 
