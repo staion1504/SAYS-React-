448e2087d24f05041293129ea86679ef2551a837
staion1504 <pagillasaitejauma@gmail.com>
2024-05-02 11:03:44 +0530

    vercel

--- React_SAYS/src/URL.js
+++ React_SAYS/src/URL.js
@@ -1,3 +1,3 @@
-const URL = "http://localhost:5000"
-// const URL = "https://says-backend.onrender.com"
+// const URL = "http://localhost:5000"
+const URL = "https://says-backend.onrender.com"
 module.exports = URL
